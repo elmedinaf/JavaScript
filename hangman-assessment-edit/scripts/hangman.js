28dde2eb7eb456d8b1a33fb587e0aa2124324d4f
Elmedina <efejzulai@students.mchenry.edu>
2024-02-06 12:15:41 -0600

    hangman

--- hangman-assessment-edit/scripts/hangman.js
+++ hangman-assessment-edit/scripts/hangman.js
@@ -1,15 +1,25 @@
-// declare and initialize array
-let game = ["COBOL", "JAVA", "PYTHON", "JAVASCRIPT"];
-let choice = Math.floor(Math.random() * 4);
+let game = ["KUROMI", "HELLO KITTY", "MY MELODY", "CINNAMOROLL", "KEROPPI", "GUDETAMA", "POCHACCO", "POMPOMPURIN", "RILAKKUMA", "PUSHEEN"];
+let choice = Math.floor(Math.random() * 10);
 let answer = game[choice];
 let myLength = answer.length;
-let display = [myLength];
+let display = Array(myLength).fill("_ ");
 let win = myLength;
 let letters = answer.split('');
 let attemptsLeft = 6;
 let output = '';
 let userLetter = '';
-// game setup works fine --steffen
+let found;
+
+let hangingImages = [
+    "01.png",
+    "02.png",
+    "03.png",
+    "04.png",
+    "05.png",
+    "06.png",
+    "07.png",
+];
+
 function setup() {
     alert(answer);
     for (let i = 0; i < answer.length; i++) {
@@ -19,42 +29,36 @@ function setup() {
     document.getElementById("word").innerHTML = output;
 }
 
-
-
-
-// Issue below here
-// noticed if we uncomment below it reveals all letter for the hangman
-
-
-document.getElementById("submit").addEventListener("click", function(event){
-
-    event.preventDefault();
+function submitGuess() {
     output = '';
     userLetter = document.getElementById("guess").value;
-    document.getElementById("guess").value = ''; //would this reset value to null
+    document.getElementById("guess").value = '';
+
+    found = false;
 
     for (let c = 0; c < answer.length; c++) {
-        //alert(letters[c]);
         if (userLetter.toUpperCase() == letters[c]) {
             display[c] = userLetter.toUpperCase();
             win--;
+            found = true;
         }
-
         output = output + display[c] + ' ';
     }
-    
-    document.getElementById("word").innerHTML = output;
-    output = '';
-    attemptsLeft--;
+
+    if (found == false) {
+        attemptsLeft--;
+        document.getElementById("hangman").src = "images/" + hangingImages[attemptsLeft];
+    }
 
     if (win < 1) {
         document.getElementById("guesses").innerHTML = 'YOU WIN!!!';
     } else if (attemptsLeft < 1) {
-        document.getElementById("guesses") = 'YOU LOSE!!!';
+        document.getElementById("guesses").innerHTML = 'YOU LOSE!!!';
     } else {
         document.getElementById("guesses").innerHTML = 'You have ' + attemptsLeft + ' guesses left';
     }
-});
 
+    document.getElementById("word").innerHTML = output;
+    output = '';
+}
 
-//scirpt connected fine somereason the button and the script are not registering.
